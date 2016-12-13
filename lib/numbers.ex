5b603032f7180aeeab8742a288221093c79b1041
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2016-12-13 15:36:26 +0100

    Documentation. to_float.

--- lib/numbers.ex
+++ lib/numbers.ex
@@ -66,16 +66,32 @@ defmodule Numbers do
 
   end
 
+  @doc """
+  Computes the unary minus of `num`, also known as its negation.
+  """
   def minus(num = %numericType{}), do: numericType.minus(num)
   def minus(num) when is_number(num), do: Kernel.-(num)
 
-
+  @doc """
+  Computes the absolute value of `num`.
+  """
   def abs(num = %numericType{}), do: numericType.abs(num)
   def abs(num) when is_number(num), do: Kernel.abs(num)
 
   defmodule CannotConvertToFloatError do
     defexception message: "Cannot convert the specified datatype to a Float."
   end
+
+  @doc """
+  Tries to convert `num` to a built-in floating-point number.
+
+  Note that precision might be lost during this conversion.
+
+  Not all numeric data types support this conversion!
+  A `CannotConvertToFloatError` is raised if this conversion is unsupported by the passed data type.
+  """
+  def to_float(num) when is_integer(num), do: num * 1.0
+  def to_float(num) when is_float(num), do: num
   def to_float(num = %numericType{}) do
     if Kernel.function_exported?(numericType, :to_float, 1) do
       numericType.to_float(num)
@@ -84,7 +100,16 @@ defmodule Numbers do
     end
   end
 
+  @doc """
+  Power function: computes `base^exponent`,
+  where `base` is Numeric,
+  and `exponent` _has_ to be an integer.
 
+  This means that it is impossible to calculate roots by using this function.
+
+  If `base` supports direct computation of `pow`, that is used. Otherwise,
+  the Exponentiation by Squaring algorithm is used.
+  """
   def pow(base = %numericType{}, exponent) when is_integer(exponent) do
     if Kernel.function_exported?(numericType, :pow, 2) do
       numericType.pow(base, exponent)
